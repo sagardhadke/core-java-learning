1aa96dd37200641c3e7da7375602c21e98d02f65
Sagar Dhadke <peaceunique2003@gmail.com>
2026-07-02 13:13:17 +0530

    Added 1st and 2nd and 3rd question

---
+++ Questions & Exam/01_Java_Basics_1_To_5/Level-1/q3.java
@@ -0,0 +1,9 @@
+//* Declare a double variable named price and assign a decimal value, then print it.
+
+public class q3 {
+    public static void main(String[] args) {
+        double price = 26.26;
+
+        System.out.println(price);
+    }
+}
