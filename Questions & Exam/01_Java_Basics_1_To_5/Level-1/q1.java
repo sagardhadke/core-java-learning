fb3d69bd57f5d5b34afc8a46bb71ea1ba9b278a5
Sagar Dhadke <peaceunique2003@gmail.com>
2026-07-02 13:07:31 +0530

    Added 1st and 2nd question

---
+++ Questions & Exam/01_Java_Basics_1_To_5/Level-1/q1.java
@@ -0,0 +1,11 @@
+//* Declare an int variable named age, assign your age to it, and print it using System.out.println.
+
+public class q1 {
+
+    public static void main(String[] args){
+        int age = 22;
+        System.out.println("My Age: " + age);
+    }
+
+
+}
---
+++ Questions & Exam/01_Java_Basics_1_To_5/Level-1/q2.java
@@ -0,0 +1,8 @@
+//* Declare a char variable named grade and assign a single letter to it, then print it.
+
+public class q2 {
+    public static void main(String[] args) {
+        char grade = 'N';
+        System.out.println(grade);
+    }
+}
