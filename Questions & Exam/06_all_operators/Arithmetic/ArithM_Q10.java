1e79fc3cc6a239d9b4abf6d7205830f9cb491921
Sagar Dhadke <peaceunique2003@gmail.com>
2026-07-11 17:40:02 +0530

    Added Q10 comments

--- Questions & Exam/06_all_operators/Arithmetic/ArithM_Q10.java
+++ Questions & Exam/06_all_operators/Arithmetic/ArithM_Q10.java
@@ -4,16 +4,17 @@
 public class ArithM_Q10 {
     public static void main(String[] args) {
         int a = 5;
-        int b = 10;
+        int b = 10; // Declared but not used
 
-        int c = a + 2;
+        int c = a + 2;   // c = 5 + 2 = 7
 
-        c += 2; // 9
-        c -= 2; // 7
-        c *= 3; // 21
-        c /= 5; // 4
-        c %= 5; //4
-        System.out.println(c); // 4
+        c += 2;          // c = 7 + 2 = 9
+        c -= 2;          // c = 9 - 2 = 7
+        c *= 3;          // c = 7 * 3 = 21
+        c /= 5;          // c = 21 / 5 = 4 (integer division, remainder discarded)
+        c %= 5;          // c = 4 % 5 = 4 (4 divided by 5 leaves remainder 4)
+
+        System.out.println(c); // Prints 4
     }
 
 
