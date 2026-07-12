9085322645eb731fd428ccb57e7dbd21a1715087
Sagar Dhadke <peaceunique2003@gmail.com>
2026-07-12 19:49:02 +0530

    Added 6-10 Questions of Logical Operator

---
+++ Questions & Exam/06_all_operators/Logical/Logical_Q6.java
@@ -0,0 +1,13 @@
+// boolean a = true, b = false; Print: a && b, a || b, !a, !b, a && !b, !a || b.
+
+public class Logical_Q6 {
+    public static void main(String[] args) {
+        boolean a = true, b = false;
+        System.out.println(a && b);
+        System.out.println(a || b);
+        System.out.println(!a);
+        System.out.println(!b);
+        System.out.println(a && !b);
+        System.out.println(!a || b);
+    }
+}
